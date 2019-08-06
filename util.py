9d6f265731fa9d91ebaf87a52037bd1b2f3f86c1
Zhewei Sun <zheweisun@hotmail.com>
2019-08-06 17:10:38 -0400

    Update Tutorial

--- util.py
+++ util.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+ep = 0.00001
+
 def log_likelihood(p):
-    return -np.sum(np.log(p))
+    return -np.sum(np.log(p + ep))
 
 def normalize(array, axis=1):
-    denoms = np.sum(array, axis=axis)
+    denoms = np.sum(array, axis=axis) + ep
     if axis == 1:
         return array / denoms[:,np.newaxis]
     if axis == 0:
